b7ae0c25b70169996f951be9c98949c144bcaee4
mhmestb2018 <74905349+mhmestb2018@users.noreply.github.com>
2024-06-16 08:12:11 -0700

    Update login-btn.tsx

--- src/app/components/login-btn.tsx
+++ src/app/components/login-btn.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 const baseBtnStyle ="bg-slate-100 hover:bg-slate-200 text-black px-6 py-2 rounded-md capitalize font-bold mt-1";
-
+const router = useRouter();
 export default function LoginBtn() {
   const { data: session, status } = useSession();
   const router = useRouter();
